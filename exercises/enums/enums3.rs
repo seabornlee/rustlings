74ba46cefaee0b05b4a92a3123406a5013ecd9a1
Seaborn Lee <seabornlee.cn@gmail.com>
2021-04-22 08:22:45 +0800

    exercises

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Quit
 }
 
 struct Point {
@@ -37,6 +39,21 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        // https://www.runoob.com/rust/rust-enum.html
+        match message {
+            Message::Move(point) =>  {
+                self.move_position(point);
+            },
+            Message::ChangeColor(color) => {
+                self.change_color(color);
+            },
+            Message::Echo(s) => {
+                self.echo(s);
+            },
+            Message::Quit => {
+                self.quit();
+            }
+        }
     }
 }
 
